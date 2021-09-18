4cac3119974071c0611bc1f4db2872d9c7321521
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2021-09-18 11:08:42 +0300

    sentry releases

--- lib/utils/sentry.ts
+++ lib/utils/sentry.ts
@@ -2,4 +2,5 @@ export const Sentry = require("@sentry/node");
 Sentry.init({
     dsn: process.env["SENTRY_CLIENT_KEY"],
     tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.3 : 1.0,
+    release: "v0.1.0"
 });
