a40fd2952f90462410bc89c0d486777f051a64c6
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2023-07-05 20:55:33 +0200

    reviews2

--- lib/models/MentorClientRequest.ts
+++ lib/models/MentorClientRequest.ts
@@ -29,10 +29,12 @@ export class MentorClientRequest {
         this.id = record.id;
         this.name = record.get("Name");
         this.email = record.get("Email");
-        this.telegram = (record.get("Telegram") as string).replace('@','');
+        this.telegram = (record.get("Telegram") as string).replace('@','') || '-';
         this.details = record.get("Description");
         this.level = record.get("Level");
-        this.review = record.get("Review");
+        const review = record.get("Review") as string;
+        const review_ng = record.get("Review2") as string;
+        this.review = review_ng || review;
         this.status = record.get("Status") as MentorClientRequestStatus;
         this.createdAt = new Date(record.get("Created Time"));
         this.modifiedAt = new Date(record.get("Last Modified Time"));
